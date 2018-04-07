52e359929e58cae2a2bdcc11d45efa5d46f82a49
Heirian <shinkcc@gmail.com>
2018-04-07 15:35:40 -0300

    Adds cards tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,12 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 20 cards" do
+    assert length(Cards.create_deck) == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
   end
 end
